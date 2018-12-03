4b1f5f6560e165a819d1c989f78455674e3899a6
zhuo <lwzhuo@live.com>
2018-12-03 22:05:11 +0800

    需求计划单完成

---
+++ src/main/java/PurchaseSystem/service/IneedPlanFormService.java
@@ -0,0 +1,17 @@
+package PurchaseSystem.service;
+
+import PurchaseSystem.model.Form.NeedPlanForm;
+
+import java.util.List;
+import java.util.Map;
+
+public interface IneedPlanFormService {
+    public int addNPF(NeedPlanForm npf);
+    public int deleteNPF(int id);
+    public int deleteNPFBatch(List<Integer> deleteList);
+    public int updateNPF(NeedPlanForm npf);
+    public int updateNPFBatch(List<NeedPlanForm> npfList);
+    public Map getBriefNPFBatch(int base,int offset);//没有详细的具体货物信息，只是摘要
+    public Map getNPFDetailById(int id);//获得某一需求计划单的详情货物信息
+    public int getCount();
+}
---
+++ src/main/java/PurchaseSystem/service/implement/form/needPlanFormImpl.java
@@ -0,0 +1,120 @@
+package PurchaseSystem.service.implement.form;
+
+import PurchaseSystem.dao.Form.needPlanFormDao;
+import PurchaseSystem.dao.Form.needPlanFormDetailDao;
+import PurchaseSystem.model.Form.NeedPlanForm;
+import PurchaseSystem.service.IneedPlanFormService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionAspectSupport;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+@Service("needPlanFormService")
+public class needPlanFormImpl implements IneedPlanFormService {
+    @Resource
+    private needPlanFormDao npfDao;
+    @Resource
+    private needPlanFormDetailDao npfdDao;
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public int addNPF(NeedPlanForm npf){
+        int num=0;
+        try {
+            List detailList = npf.getDetailList();
+            npfDao.insertNPForm(npf);
+            npfdDao.insertDetail(detailList,npf.getId());
+        }catch (Exception e){
+            e.printStackTrace();
+            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();//手动执行回滚
+            num=-1;
+        }
+        return num;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public int deleteNPF(int id){
+        int num=0;
+        try {
+            npfdDao.deleteDetail(id);
+            npfDao.deleteNPForm(id);
+        }catch (Exception e){
+            e.printStackTrace();
+            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+            num=-1;
+        }
+        return num;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public int deleteNPFBatch(List<Integer> deleteList){
+        int num=0;
+        try {
+            for(int id:deleteList){
+                npfdDao.deleteDetail(id);
+                npfDao.deleteNPForm(id);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+            num=-1;
+        }
+        return num;
+    }
+    @Transactional(propagation = Propagation.REQUIRED)
+    public int updateNPF(NeedPlanForm npf){
+        int num=0;
+        try {
+            npfDao.updateNPForm(npf);
+            if(npf.getDetailList()!=null)
+                npfdDao.updateDetail(npf.getDetailList());
+        }catch (Exception e) {
+            e.printStackTrace();
+            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+            num = -1;
+        }
+        return num;
+    }
+    @Transactional(propagation = Propagation.REQUIRED)
+    public int updateNPFBatch(List<NeedPlanForm> npfList){
+        int num=0;
+        try {
+            Iterator it = npfList.iterator();
+            while (it.hasNext()){
+                NeedPlanForm npf = (NeedPlanForm)it.next();
+                npfDao.updateNPForm(npf);
+                if(npf.getDetailList()!=null)
+                    npfdDao.updateDetail(npf.getDetailList());
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+            num=-1;
+        }
+        return num;
+    }
+    public HashMap getBriefNPFBatch(int base, int offset){//没有详细的具体货物信息，只是摘要
+        HashMap map = new HashMap();
+        List list = npfDao.selectNPFormBatch(base,offset);
+        map.put("npfList",list);
+        map.put("batchNum",list.size());
+        return map;
+    }
+
+    public HashMap getNPFDetailById(int id){//获得某一需求计划单的详情货物信息
+        HashMap map = new HashMap();
+        List list = npfdDao.selectDetailByFormId(id);
+        map.put("detailList",list);
+        map.put("num",list.size());
+        map.put("formId",id);
+        return map;
+    }
+    public int getCount(){
+        return npfDao.getCount();
+    }
+}
